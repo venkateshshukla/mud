b5c40292c9a145a411a5f24534a2b8a2228fcd23
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2016-03-13 20:02:49 +0530

    Logging and Jackson
    Log the requests coming in.
    Respond with json generated related to POJO using jackson.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- mud-api/src/main/java/in/vshukla/MudServlet.java
+++ mud-api/src/main/java/in/vshukla/MudServlet.java
@@ -1,6 +1,6 @@
 package in.vshukla;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,41 +10,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
 
 public class MudServlet extends HttpServlet {
 
+    private static final Logger LOGGER = Logger.getLogger(MudServlet.class.getName());
+
     private Person person;
+    private ObjectMapper mapper;
 
     @Override
     public void init() throws ServletException {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/spring_config.xml");
         person = (Person) applicationContext.getBean("person");
+        mapper = (ObjectMapper) applicationContext.getBean("objectMapper");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        addResponse(req, resp);
+        LOGGER.info(String.format("Received a %s request.", req.getMethod()));
+        addResponse(resp);
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        addResponse(req, resp);
+        LOGGER.info(String.format("Received a %s request.", req.getMethod()));
+        addResponse(resp);
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        addResponse(req, resp);
+        LOGGER.info(String.format("Received a %s request.", req.getMethod()));
+        addResponse(resp);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        addResponse(req, resp);
+        LOGGER.info(String.format("Received a %s request.", req.getMethod()));
+        addResponse(resp);
     }
 
-    private void addResponse(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    private void addResponse(HttpServletResponse resp) throws IOException {
         PrintWriter pw = resp.getWriter();
-        pw.write(String.format("Received a %s request.\n", req.getMethod()));
-        pw.write(person.toString());
+        pw.write(mapper.writeValueAsString(person));
         pw.close();
     }
 }
